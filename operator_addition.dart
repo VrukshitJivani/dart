d48c14c09bb2c926fb46b4a0712cfc5c4a3669f4
Vrukshit Jivani <vjivani2233@gmail.com>
2026-01-05 20:30:28 +0530

    Add files via upload

---
+++ operator_addition.dart
@@ -0,0 +1,13 @@
+void main()
+{
+    int a=90;
+    int b=89;
+
+    print(a+b);
+    print(a-b);
+    print(a*b);
+    print(a/b);
+    print(a%b);
+    print(a~/b);// Integer Division :it can return only integer ,it can remove decimal numbers
+
+}
---
+++ operator_relational.dart
@@ -0,0 +1,18 @@
+void main()
+{
+    int a=79;
+    int b=90;
+    //>
+    if(a>b)//it return boolean value//flase
+    {
+        print("a is gretter");
+    }
+    else
+     {
+        print("b is gretter");
+     }
+    //==
+     if(a==b)=>print("a is equal to b");
+     else=>print("is not equal");
+
+}

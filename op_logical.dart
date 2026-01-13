b6ea7d14cca5a9e46aff35a15b4d6bd0de57fe63
Vrukshit Jivani <vjivani2233@gmail.com>
2026-01-13 16:39:25 +0530

    Add files via upload

---
+++ op_logical.dart
@@ -0,0 +1,16 @@
+void main()
+{
+    print("Logical Operator :");
+    int a=5,b=3;
+    //&&(and)
+    bool s=(a==5 && b==3);//true
+    print(s);
+
+    //||(or)
+    s=(a>0 || b==6);//true
+    print(s);
+
+    //(!)not
+    s=(a!=5);
+    print(s);
+}
